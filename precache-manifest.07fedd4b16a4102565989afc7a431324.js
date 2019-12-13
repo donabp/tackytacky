2734e19b82969fb31e291bab2f4f69e288edce96
Dona Pop <dona.pop@maxxpotential.com>
2019-12-13 10:24:57 -0500

    Updates

---
+++ precache-manifest.07fedd4b16a4102565989afc7a431324.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781beb03d319e733f167b3d7846b3a48",
+    "url": "/tackytacky/index.html"
+  },
+  {
+    "revision": "ffd598e2270d44a38904",
+    "url": "/tackytacky/static/css/main.36e8063c.chunk.css"
+  },
+  {
+    "revision": "68d3b11eecba41da96d8",
+    "url": "/tackytacky/static/js/2.6d1731aa.chunk.js"
+  },
+  {
+    "revision": "dd5a792277257f6ffba38806fc2e8b66",
+    "url": "/tackytacky/static/js/2.6d1731aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffd598e2270d44a38904",
+    "url": "/tackytacky/static/js/main.d6c653d1.chunk.js"
+  },
+  {
+    "revision": "8544a4cf285451ae5d3d",
+    "url": "/tackytacky/static/js/runtime-main.e70cff28.js"
+  },
+  {
+    "revision": "19f84676dc8dfa2084596c616474b689",
+    "url": "/tackytacky/static/media/countDown.19f84676.mp4"
+  },
+  {
+    "revision": "de9bdd024b8b6b31a0907cd9e22db6f7",
+    "url": "/tackytacky/static/media/feliz3.de9bdd02.mp3"
+  },
+  {
+    "revision": "c4ce941363d839731143f56bb63fad5a",
+    "url": "/tackytacky/static/media/pic1.c4ce9413.jpg"
+  },
+  {
+    "revision": "4999f43a038d9492dbc030c2ab2e4f5c",
+    "url": "/tackytacky/static/media/snow.4999f43a.mp3"
+  },
+  {
+    "revision": "01f4c2c77bd63c72e02b1e4b69cf0db3",
+    "url": "/tackytacky/static/media/tacky.01f4c2c7.mp4"
+  }
+]);
